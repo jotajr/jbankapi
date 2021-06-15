4613615b7265e6ae5572bf3a26fabecf24e9ccac
Jota Freitas Jr <jotafreitasjr@gmail.com>
2021-06-15 09:19:51 -0300

    first commit

---
+++ src/main/java/com/jotajr/jbankapi/JbankapiApplication.java
@@ -0,0 +1,13 @@
+package com.jotajr.jbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbankapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jotajr/jbankapi/JbankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotajr.jbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
