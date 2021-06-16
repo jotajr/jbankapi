c74d13e224715276f2e24726f2a19c0dfa14adc0
Jota Freitas Jr <jotafreitasjr@gmail.com>
2021-06-15 21:56:53 -0300

    Ajustado script sql

--- sql/banco.sql
+++ sql/banco.sql
@@ -1,6 +1,6 @@
 create table agencia
 (
-    id_agencia int not null,
+    id_agencia int auto_increment,
     nome_agencia varchar(45) not null,
     endereco varchar(45) not null,
     telefone varchar(20) not null,
